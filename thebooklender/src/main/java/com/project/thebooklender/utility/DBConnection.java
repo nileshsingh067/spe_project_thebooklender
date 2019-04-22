b96f8aad968c59fe0548293c9ffe55e137dd77cb
kotlasaicharanreddy <kotlasaicharan@yahoo.com>
2019-04-23 00:45:33 +0530

    test commit 1(changed hostname in dbconnection.java)

--- thebooklender/src/main/java/com/project/thebooklender/utility/DBConnection.java
+++ thebooklender/src/main/java/com/project/thebooklender/utility/DBConnection.java
@@ -12,7 +12,7 @@ public class DBConnection {
 			Connection con=null;
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
-				con=DriverManager.getConnection("jdbc:mysql://localhost:3306/booklender", "root", "root"
+				con=DriverManager.getConnection("jdbc:mysql://localhost1:3306/booklender", "root", "root"
 						+ "");
 				System.out.println(logHead+" Mysql Connection Created Successfully ..");
 			}catch(Exception e) {
